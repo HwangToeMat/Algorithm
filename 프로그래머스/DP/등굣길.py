0306faf6391d4ea84773eb7ef2ee7ea2e83cc1b2
HwangToeMat <46777602+HwangToeMat@users.noreply.github.com>
2020-09-27 00:58:40 +0900

    Create 등굣길.py

---
+++ 프로그래머스/DP/등굣길.py
@@ -0,0 +1,16 @@
+def solution(m, n, puddles):
+    dp = [[0 for i in range(m+1)] for j in range(n+1)]
+    for x, y in puddles:
+        dp[y][x] = 'pad'
+    dp[1][1] = 1
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i == 1 and j == 1:
+                continue
+            if dp[i][j] == 'pad':
+                dp[i][j] = 0
+                continue
+            tmp = dp[i-1][j] + dp[i][j-1]
+            dp[i][j] = tmp
+    end = dp[-1][-1]
+    return end
